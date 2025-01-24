68460eefffd007b490ef99763188b5da61179ce3
Skytestpro <chiisa42te@gmail.com>
2025-01-24 17:30:44 +0500

    lesson_3_n2

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,10 +1,10 @@
 from smartphone import Smartphone
 
-catalog = [Smartphone('Xiaomi', '14T Pro', '7 999 999 99 99'),
-           Smartphone('Honor', 'X7c', '7 999 999 99 00'),
-           Smartphone('Samsung', 'S24 5G', '7 999 999 00 00'),
-           Smartphone('Apple', 'iPhone 15 Pro', '7 999 900 00 00'),
-           Smartphone('Infinix', 'GT 20 Pro', '7 999 000 00 00')]
+catalog = [Smartphone('Xiaomi', '14T Pro', '+79999999999'),
+           Smartphone('Honor', 'X7c', '+79999999900'),
+           Smartphone('Samsung', 'S24 5G', '+79999990000'),
+           Smartphone('Apple', 'iPhone 15 Pro', '+79999000000'),
+           Smartphone('Infinix', 'GT 20 Pro', '+79990000000')]
 
 for phone in catalog:
     print(f'{phone.brand} - {phone.model}. {phone.number}')
