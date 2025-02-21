a09208dfa4a8629fe06ff6b8887a95d39204e05c
Skytestpro <chiisa42te@gmail.com>
2025-02-21 12:18:38 +0500

    lesson_7_2

--- 07_lesson/1_form/form_test.py
+++ 07_lesson/1_form/form_test.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from classes.main_page_class import MainPage
+from classes.result_page_class import ResultPage
+
 
 def test_form():
     browser = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install())
         )
-    from classes.main_page_class import MainPage
     main = MainPage(browser)
     main.get_page()
     main.find_and_send_elements()
     main.find_and_click_btn()
 
-    from classes.result_page_class import ResultPage
     result = ResultPage(browser)
 
     for m in result.class_result_form():
--- 07_lesson/2_calculator/calculator_test.py
+++ 07_lesson/2_calculator/calculator_test.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from classes.calculator_class import Calculator
+
 
 def test_calculator():
     browser = webdriver.Chrome(
         service=ChromeService(ChromeDriverManager().install())
         )
-    from classes.calculator_class import Calculator
     calc = Calculator(browser, 45)
     calc.get_page()
     calc.counter()
